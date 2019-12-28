e4f6c567804185daf513ce414a3c1148a6751b31
Amardeep Singh <amardeep.singh00@gmail.com>
2019-12-27 20:14:20 -0800

    Revert "Revert "Tree prototyoe for demo""
    This reverts commit 050aa4d0b7362f72d1235d708d0505b3331bfc39.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { render } from "react-dom";
+import DataTree from './components/DataTree';
+import Modal from './components/Modal';
 
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<DataTree/>, document.getElementById('root'));
