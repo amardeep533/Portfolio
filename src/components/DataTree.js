050aa4d0b7362f72d1235d708d0505b3331bfc39
Amardeep Singh <amardeep.singh00@gmail.com>
2019-12-27 20:05:22 -0800

    Revert "Tree prototyoe for demo"
    This reverts commit 894a355c0a4d1110be3e2047dfe4f7f76bade1a3.

--- src/components/DataTree.js
+++ src/components/DataTree.js
@@ -1,82 +1,62 @@
 import React, { Component } from 'react';
 import Tree from 'react-tree-graph';
 import { easeElastic } from 'd3-ease';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
 
 const data = {
     name: 'Parent',
-    textProps: { x: -30, y: 25 },
     children: [{
-        name: 'Child One',
-        textProps: { x: -30, y: 25 },
+        name: 'Child One'
     }, {
         name: 'Child Two',
-        textProps: { x: -25, y: 25 },
+        gProps: {
+            className: 'red-node',
+            onClick: (event, node) =>
+                alert(`Clicked ${node}!`)
+        }
     },
     {
         name: 'Child Three',
-        textProps: { x: -25, y: 25 },
         children: [{
-            name: 'Grand Child One',
-            textProps: { x: -25, y: 25 },
+            name: 'Grand Child One'
         }, {
             name: 'Grand Child Two',
-            textProps: { x: -25, y: 25 },
+            gProps: {
+                className: 'red-node',
+                onClick: (event, node) =>
+                    alert(`Clicked ${node}!`)
+            }
         },
         {
-            name: 'Grand Child Three',
-            textProps: { x: -25, y: 25 },
+            name: ' Grand Child Three',
             children: [{
-                name: 'Leaf One',
-                textProps: { x: -25, y: 25 },
+                name: 'Leaf One'
             }, {
                 name: 'Leaf Two',
-                textProps: { x: -25, y: 25 },
+                gProps: {
+                    className: 'red-node',
+                    onClick: (event, node) =>
+                        alert(`Clicked ${node}!`)
+                }
             },
             {
-                name: 'Leaf Three',
-                textProps: { x: -25, y: 25 },
+                name: 'Leaf Three'
             }]
         }]
     }]
 };
-
-const handleClick = (event, node,type) => {
-    return (Swal.fire({
-        title: `Add a child node for ${node} and type ${type}`,
-        input: 'text',
-        inputValue: '',
-        showCancelButton: true,
-        inputValidator: (value) => {
-            if (!value) {
-                return 'You need to write something!'
-            } else if('ABC' !== value){
-                return 'You can only enter `ABC`'
-            } else {
-                return Swal.fire(`You added a grand child ${value}`)
-            }
-        }
-    }))
-};
 export default class DataTree extends Component {
     render() {
-        return <Tree
-                data={data}
-                height={700}
-                width={1000}
-                animated
-                duration={1000}
-                easing={easeElastic}
-                nodeRadius={15}
-                margins={{ top: 20, bottom: 10, left: 200, right: 200 }}
-                gProps={{
-                    className: 'node',
-                    onClick: handleClick
-                }}
-                svgProps={{
-                    className: 'custom'
-                }}
-            />;
+        return <div className="custom-container">
+        <Tree
+            data={data}
+            height={600}
+            width={800}
+            animated
+            duration={1000}
+            easing={easeElastic}
+            svgProps={{
+                className: 'custom'
+            }} />
+    </div>;
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { render } from "react-dom";
-import DataTree from './components/DataTree';
-import Modal from './components/Modal';
 
 
 
-ReactDOM.render(<DataTree/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
